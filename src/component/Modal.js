d88485d9da1395c6e720e3a8dc5efcb332617dd8
Lucas0105 <dnjswo105@gmail.com>
2022-06-23 04:28:00 +0900

    add alarm modal

---
+++ src/component/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Modal.css';
+
+
+const Modal = (props) => {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close, header } = props;
+
+  return (
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? 'openModal modal' : 'modal'}>
+      {open ? (
+        <section>
+          <h2>
+            test
+          </h2>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+
+export default Modal;
--- src/component/navigation.js
+++ src/component/navigation.js
@@ -1,8 +1,33 @@
 import './navigation.css';
+import Modal  from './Modal';
+import React, { useEffect, useState, useRef} from 'react';
 
-function navigation() {
+function Navigation(props) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalEl = useRef();
+
+  useEffect(() => {
+    document.addEventListener('click', clickModalOutside);
+
+    return () => {
+      document.removeEventListener('click', clickModalOutside);
+    };
+  });
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+  const clickModalOutside = event => {
+    if (modalOpen && event.target.className==="openModal modal") {
+      setModalOpen(false);
+    }
+  };
     return (
       <div id="navigation">
+ 
           <div className='logoFrame'>
               <div className='logo'>
                 <img alt='logo img'></img>
@@ -13,12 +38,23 @@ function navigation() {
               </div>
           </div>
           <div className='loginFrame'>
-              <div className='login-join'>
+              {/* <div className='login-join'>
                 로그인/회원가입
+              </div> */}
+              <div className='login-text'>| 마이페이지 | <span className='alarm' onClick={openModal}>알림</span> | </div>
+              <div className='logout'>
+                로그아웃
               </div>
-          </div>
+             </div>
+             
+
+              <Modal ref={modalEl} open={modalOpen} close={closeModal} header="Modal heading">
+                <main> {props.children} </main>에 내용이 입력된다. 리액트 함수형 모달
+                팝업창입니다. 쉽게 만들 수 있어요. 같이 만들어봐요!
+              </Modal>
+            
       </div>
     );
   }
   
-  export default navigation;
+  export default Navigation;
