7b869c9d853dbebcd8dffdabb8eeb82b3daf6b65
Lucas0105 <dnjswo105@gmail.com>
2022-06-22 20:46:48 +0900

    fix router func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import MainPage from "./pages/main/mainPage";
 import LoginPage from "./pages/login/loginPage";
 import RegisterPage from "./pages/register/registerPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
