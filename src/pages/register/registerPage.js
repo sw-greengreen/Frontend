cdf4bd80a656f72ca2ea1b2ad030bf3a3c027663
favorcat <favorcat.me@gmail.com>
2022-06-23 03:12:51 +0900

    add registerPage html css (#2)

--- src/pages/register/registerPage.js
+++ src/pages/register/registerPage.js
@@ -69,13 +69,13 @@ function RegisterPage() {
               <div class="text">아이디</div>
               <div class="requiredText">*</div>
             </div>
-            <input type={"text"} class="register__input" autoComplete="username" placeholder="" pattern="^[A-Za-z0-9]{6,20}$" required onChange={(e) =>{setUser({...User, "username" : e.target.value})}}></input>
+            <input type={"text"} class="register__input" autoComplete="username" placeholder="최소 6자" pattern="^[A-Za-z0-9]{6,20}$" required onChange={(e) =>{setUser({...User, "username" : e.target.value})}}></input>
 
             <div class="textWrapper">
               <div class="text">비밀번호</div>
               <div class="requiredText">*</div>
             </div>
-            <input type={"password"} class="register__input" autoComplete="current-password" placeholder="숫자, 영문, 최소8자" pattern="^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$" required onChange={(e) =>{setUser({...User, "password" : e.target.value})}}></input>
+            <input type={"password"} class="register__input" autoComplete="current-password" placeholder="숫자, 영문, 최소 8자" pattern="^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$" required onChange={(e) =>{setUser({...User, "password" : e.target.value})}}></input>
             <input type={"password"} class="register__input" autoComplete="current-password-check" placeholder="비밀번호 확인" required onChange={(e) =>{setConfirmPW(e.target.value);}}></input>
           </div>
 
