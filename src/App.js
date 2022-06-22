0f953d38b3315b7c494fe72d1c1bc1e8669cc6b6
Lucas0105 <dnjswo105@gmail.com>
2022-06-22 21:45:56 +0900

    add navigation html css without logo img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import MainPage from "./pages/main/mainPage";
 import LoginPage from "./pages/login/loginPage";
 import RegisterPage from "./pages/register/registerPage";
+import Navigation from "./component/navigation";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
@@ -8,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navigation /> */}
+        <Navigation />
 
         <Routes>
           <Route path="/" element={<MainPage />} />
