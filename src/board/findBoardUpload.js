5444321db62a234b0938e8a44050231391e0e129
Lucas0105 <dnjswo105@gmail.com>
2022-06-23 22:36:26 +0900

    add picture

--- src/board/findBoardUpload.js
+++ src/board/findBoardUpload.js
@@ -1,6 +1,53 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { call } from '../hooks/usefetch';
 import './findBoardUpload.css';
 
-function findBoardUpload() {
+function FindBoardUpload() {
+
+  const [post, setPost] = new useState({"username":"username1", "title":"", "content":"", "photo" : "", "hashtag":"", "postType":"DISCOVERY", "resolvingStatus":"WAITING", "isAnontymous":""})
+  const [imgBase64, setImgBase64] = useState(""); // 파일 base64
+  const [imgFile, setImgFile] = useState(null);	//파일	
+
+const handleChangeFile = (event) => {
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      // 2. 읽기가 완료되면 아래코드가 실행됩니다.
+      const base64 = reader.result;
+      if (base64) {
+        setImgBase64(base64.toString()); // 파일 base64 상태 업데이트
+      }
+    }
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
+      setImgFile(event.target.files[0]); // 파일 상태 업데이트
+
+    }
+
+  }
+
+
+  const handleClick = () =>{
+    const formdata = new FormData();
+    console.log(imgFile);
+    formdata.append("file", imgFile);
+
+    const config = {
+      Headers: {
+        'content-type':'multipart/form-data',
+      },
+    };
+    axios.post("http://localhost:8080/api/v1/file", formdata, config).then((res) => {console.log(res)});
+  }
+
+  const submitBtn = ()=>{
+
+     call("/api/v1/post", "POST", post).then((response)=>{
+      
+      console.log(response)})
+      console.log(post);
+
+  }
     return (
       <div id="findBoardUpload">
         <div className='boardFrame'>
@@ -15,27 +62,26 @@ function findBoardUpload() {
         <div className='board-body'>
           상품이미지<span style={{color:"#12DB64", marginRight:"5%"}}>*</span>
           <div className='picture-frame'>
-            <img src='/img/test_img.jpg'></img>
-
+          {imgBase64 && <img src={imgBase64} alt="preview-img" />}
           </div>
 
-          
+          <form className='upload_input'>
+            <input type="file" id="image" onChange={handleChangeFile}></input>
+        </form>
         </div>
+
         <div className='board-hash'>
               <h5>#해시태그</h5>
               <div className='board-hashtags'>
-                <input className='board-hashtag' type="text" placeholder='#해시태그 작성 후 Enter'></input>
-                
+                <input onChange={(e)=>{setPost({...post, "hashtag" : e.target.value})}} className='board-hashtag' type="text" placeholder='#해시태그 작성 후 Enter'></input>
                 </div>
               <hr></hr>
               <div>
-              <div className='title'><p>제목</p><span style={{color:"#12DB64", marginRight:"10%"}}>*</span> <input placeholder='제목을 작성해주세요' type="text"></input></div>
+              <div className='title'><p>제목</p><span style={{color:"#12DB64", marginRight:"10%"}}>*</span> <input onChange={(e)=>{setPost({...post, "title" : e.target.value})}} placeholder='제목을 작성해주세요' type="text"></input></div>
 
-              <div className='content'><p>내용</p><span style={{color:"#12DB64", marginRight:"10%"}}>*</span><textarea placeholder='내용을 작성해주세요'></textarea></div>
-              
-
+              <div className='content'><p>내용</p><span style={{color:"#12DB64", marginRight:"10%"}}>*</span><textarea onChange={(e)=>{setPost({...post, "content" : e.target.value})}} placeholder='내용을 작성해주세요'></textarea></div>
               </div>
-              <button>작성완료</button>
+              <button onClick={handleClick}>작성완료</button>
           </div>
 
          
@@ -43,4 +89,4 @@ function findBoardUpload() {
     );
   }
   
-  export default findBoardUpload;
+  export default FindBoardUpload;
