a0caf078251d779f32eab83f84250914b415862a
Lucas0105 <dnjswo105@gmail.com>
2022-06-23 13:55:43 +0900

    fix boardUpload css

--- src/board/boardUpload.js
+++ src/board/boardUpload.js
@@ -24,22 +24,9 @@ function boardUpload() {
         <div className='board-hash'>
               <h5>#해시태그</h5>
               <div className='board-hashtags'>
-                <div className='board-hashtag'>#멀티태그</div>
-                <div className='board-hashtag'>#멀티태그</div>
-                <div className='board-hashtag-empty'></div>
-                <div className='board-hashtag-empty'></div>
-                <div className='board-hashtag-empty'></div>
-                <br/>
+                <input className='board-hashtag' type="text" placeholder='#해시태그 작성 후 Enter'></input>
                 
-                </div>
-                <div className='board-hashtags last-tags'>
-                <div className='board-hashtag'>#멀티태그</div>
-                <div className='board-hashtag'>#멀티태그</div>
-                <div className='board-hashtag-empty'></div>
-                <div className='board-hashtag-empty'></div>
-                <div className='board-hashtag-empty'></div>
-                <br/>
-                
+
                 </div>
               <hr></hr>
               <div>
